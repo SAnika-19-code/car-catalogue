d72667f0b88edd60e61cd095c9825441dfc32cbd
Sanika Gaikwad <19112007sanikagaikwad@gmail.com>
2026-05-26 16:09:58 +0530

    Moved Firebase config to env variables

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,12 +4,13 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD1LLK5Mnp6Ruc4goeurfeQ7Rqgi-p7vtY",
-  authDomain: "car-catalogue-fdce6.firebaseapp.com",
-  projectId: "car-catalogue-fdce6",
-  storageBucket: "car-catalogue-fdce6.firebasestorage.app",
-  messagingSenderId: "350430244534",
-  appId: "1:350430244534:web:52099c83bba2d64cd00017"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:
+    process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
